ee18c7f2e0df304369c0909b19bbaca655cebbca
olegtr <oleg.trygub@hotmail.com>
2016-04-02 13:03:14 -0700

    second version, minor bugs fixed

--- src/average_degree.py
+++ src/average_degree.py
@@ -2,8 +2,8 @@ import sys, json, time
 import heapq
 
 class DegreeCalculator:
-    tweetsHeap = []
-    hashtagsGraph = {}
+    tweets_heap = []
+    hashtags_graph = {}
 
     @staticmethod
     def tweets_belong_to_the_same_window(timestamp1, timestamp2):
@@ -11,22 +11,22 @@ class DegreeCalculator:
          return  difference <= 60.0 and difference >= 0
 
     def add_vertex_to_the_graph(self, hashtag1, hashtag2):
-        if hashtag1 not in self.hashtagsGraph:
-            self.hashtagsGraph[hashtag1] = {hashtag2: 1}
-        elif hashtag2 not in self.hashtagsGraph[hashtag1]:
-            self.hashtagsGraph[hashtag1][hashtag2] = 1
+        if hashtag1 not in self.hashtags_graph:
+            self.hashtags_graph[hashtag1] = {hashtag2: 1}
+        elif hashtag2 not in self.hashtags_graph[hashtag1]:
+            self.hashtags_graph[hashtag1][hashtag2] = 1
         else:
-            self.hashtagsGraph[hashtag1][hashtag2] += 1
+            self.hashtags_graph[hashtag1][hashtag2] += 1
 
     def remove_vertex_from_the_graph(self, hashtag1, hashtag2):
-        connected_nodes = self.hashtagsGraph[hashtag1]
+        connected_nodes = self.hashtags_graph[hashtag1]
         edge_weight = connected_nodes[hashtag2]
         if edge_weight <= 1:
             del connected_nodes[hashtag2]
         else:
             connected_nodes[hashtag2] = edge_weight - 1
         if not connected_nodes:
-            del self.hashtagsGraph[hashtag1]
+            del self.hashtags_graph[hashtag1]
 
     def add_hashtags_to_the_graph(self, hashtags):
          for hashtag_start in hashtags:
@@ -42,38 +42,48 @@ class DegreeCalculator:
                     self.remove_hashtags_from_the_graph(hashtag_start, hashtag_end)
 
     def process_tweet_data(self, hashtags, timestamp):
-        while self.tweetsHeap and not DegreeCalculator.tweets_belong_to_the_same_window(timestamp, self.tweetsHeap[0][0]):
-            tweet_data = heapq.heappop(self.tweetsHeap)
+        while self.tweets_heap and not DegreeCalculator.tweets_belong_to_the_same_window(timestamp, self.tweets_heap[0][0]):
+            tweet_data = heapq.heappop(self.tweets_heap)
             self.remove_hashtags_from_the_graph(tweet_data[1])
         if len(hashtags) > 1:
-            heapq.heappush(self.tweetsHeap, (timestamp, hashtags))
+            heapq.heappush(self.tweets_heap, (timestamp, hashtags))
             self.add_hashtags_to_the_graph(hashtags)
 
-        print self.hashtagsGraph
+        #print self.hashtags_graph
 
     def get_average_degree(self):
         sum = 0
-        vertices = self.hashtagsGraph.keys()
+        vertices = self.hashtags_graph.keys()
         for vertex in vertices:
-            sum += len(self.hashtagsGraph[vertex])
-        return sum / len(vertices)
-
-
+            sum += len(self.hashtags_graph[vertex])
+        return sum / float(len(vertices))
 
 
 input_path = sys.argv[1]
 output_path = sys.argv[2]
 
-degreeCalculator = DegreeCalculator()
+degree_calculator = DegreeCalculator()
 
-with open(input_path) as input_file:
+with open(input_path) as input_file, open(output_path, 'w') as output_file:
     for line in input_file:
         data = json.loads(line)
-        currentHashtags = map(lambda hash: hash[u"text"], data[u"entities"][u"hashtags"])
-        currentTimestamp = time.strptime(data[u"created_at"], "%a %b %d %H:%M:%S +0000 %Y")
-        print currentHashtags
-        degreeCalculator.process_tweet_data(currentHashtags, currentTimestamp)
-        print degreeCalculator.get_average_degree()
+        if not data[u"entities"] or not data[u"entities"][u"hashtags"] or not data[u"created_at"]:
+            continue
+        current_hashtags = map(lambda hashtag: hashtag[u"text"], data[u"entities"][u"hashtags"])
+        current_timestamp = time.strptime(data[u"created_at"], "%a %b %d %H:%M:%S +0000 %Y")
+        #print current_hashtags
+
+        degree_calculator.process_tweet_data(current_hashtags, current_timestamp)
+        average_degree = degree_calculator.get_average_degree()
+        #print average_degree
+        output_file.write('%.2f' % average_degree + '\n')
+
+    output_file.close()
+
+
+
+
+
 
 
 
